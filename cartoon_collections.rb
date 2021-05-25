032a4158dfa0cc71ee1275b7f57ba2814aa0aef4
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-25 01:17:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  array.map do |call|
+  planeteer_calls.map do |call|
     call.capitalize.concat("!")
   end
 end
@@ -17,7 +17,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  
+  a
 end
 
 def find_valid_calls(planeteer_calls)
